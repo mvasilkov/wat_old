13f61ed54913df8b4a00dfa0d74fc047dbe6eaf7
Mark Vasilkov <mvasilkov@gmail.com>
2013-08-27 22:25:45 +0300

    Usage

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
-var git = require('./lib/git')
+var package = require('./package.json'),
+    path = require('path'),
+    program = require('commander'),
+    git = require('./lib/git')
 
 function main() {
-    console.log('wat')
+    program
+        .option('-r, --repo <dir>', 'git repository to use',
+                path.resolve, process.cwd())
+        .version(package.version)
+        .parse(process.argv)
+
+    if (git.isRepo(program.repo))
+        console.log('wat:', program.repo)
+    else
+        console.error('wat: err')
 }
 
 if (require.main === module) {

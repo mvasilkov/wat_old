f383d4d5951a4b66405f58d435c0fe43aeb616cf
Mark Vasilkov <mvasilkov@gmail.com>
2013-09-21 00:23:52 +0300

    util.isFile

--- test/util.js
+++ test/util.js
@@ -1,9 +1,18 @@
-var should = require('should'),
+var assert = require('assert'),
     util = require('../lib/util')
+
+function reject(a) { if (a) assert.fail(a, 'nil', '', '->') }
 
 describe('util', function () {
     it('should provide isDir', function () {
-        should.exist(util.isDir('/bin'))
-        should.not.exist(util.isDir('/WINNT'))
+        assert(util.isDir('/bin'))    // dir
+        reject(util.isDir('/bin/sh')) // file
+        reject(util.isDir('/WINNT'))  // n/a
+    })
+
+    it('should provide isFile', function () {
+        reject(util.isFile('/bin'))    // dir
+        assert(util.isFile('/bin/sh')) // file
+        reject(util.isFile('/WINNT'))  // n/a
     })
 })

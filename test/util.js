bacc8e3bb04430916382e5365f3dbb7e5c50aaba
Mark Vasilkov <mvasilkov@gmail.com>
2013-09-21 15:17:37 +0300

    Finder

--- test/util.js
+++ test/util.js
@@ -5,14 +5,27 @@ function reject(a) { if (a) assert.fail(a, 'nil', '', '->') }
 
 describe('util', function () {
     it('should provide isDir', function () {
-        assert(util.isDir('/bin'))    // dir
-        reject(util.isDir('/bin/sh')) // file
-        reject(util.isDir('/WINNT'))  // n/a
+        assert(util.isDir('/bin'))     // dir
+        reject(util.isDir('/bin/sh'))  // file
+        reject(util.isDir('/ninjacy')) // n/a
     })
 
     it('should provide isFile', function () {
-        reject(util.isFile('/bin'))    // dir
-        assert(util.isFile('/bin/sh')) // file
-        reject(util.isFile('/WINNT'))  // n/a
+        reject(util.isFile('/bin'))     // dir
+        assert(util.isFile('/bin/sh'))  // file
+        reject(util.isFile('/jesusry')) // n/a
+    })
+
+    describe('finder', function () {
+        var dir = process.cwd(),
+            f = util.finder(dir + '/bin', dir + '/lib', dir + '/test')
+
+        it('should find files', function () {
+            assert.equal(f('wat'), dir + '/bin/wat')
+            assert.equal(f('git.js'), dir + '/lib/git.js')
+            assert.equal(f('cli.js'), dir + '/test/cli.js')
+            reject(f('app.js'))
+            reject(f('autoexec.bat'))
+        })
     })
 })

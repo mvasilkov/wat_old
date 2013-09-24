8ea55552f2cb7f9030235b5936fb89e88588f8c7
Mark Vasilkov <mvasilkov@gmail.com>
2013-09-24 23:49:00 +0300

    Render

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ var package = require('./package.json'),
     path    = require('path'),
     program = require('commander'),
     express = require('express'),
+    render  = require('./lib/render'),
     git     = require('./lib/git'),
     util    = require('./lib/util')
 
 function start(options) {
     var app = express(),
         finder = util.finder(options.repo, __dirname)
+
+    app.use(render.pages(finder))
 
     if (options.nop) return app
 
@@ -37,4 +40,5 @@ function main() { git.usable(cli) }
 
 if (require.main === module) main()
 
+module.exports.main = main
 module.exports.start = start

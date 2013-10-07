1f15b81057bbf13e45d08789aa92a380f077642c
Mark Vasilkov <mvasilkov@gmail.com>
2013-10-07 16:52:54 +0200

    model.reset

--- test/model.js
+++ test/model.js
@@ -1,9 +1,15 @@
-var assert = require('assert'),
+var assert    = require('assert'),
+    iosys     = require('iosys'),
     mediatype = require('mediatype'),
-    model = require('../lib/model')
+    model     = require('../lib/model')
 
 describe('model', function () {
+    beforeEach(model.reset)
+
     it('should provide defaults', function () {
+        var f = iosys.finder(process.cwd())
+        model.init(f)
+
         var p = model.get('')
         assert(p && p.r)
         assert.equal(mediatype.lookup(p.rt || p.r), 'text/html')
